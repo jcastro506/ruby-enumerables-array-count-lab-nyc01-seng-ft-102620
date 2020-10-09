97deeea7d63aa5d1b84561993e3e2149625e3d16
Josh Castro <castrojoshua805@gmail.com>
2020-10-09 00:05:19 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,11 +1,7 @@
 def count_strings(array)
-  array.count do |words|
-   words.class == String
- end 
+  # Return the total number of strings in the provided array using the count enumerable
 end
 
 def count_empty_strings(array)
-  array.count do |words| 
-    words.class == String && words.length == 0   
-  end 
-end
+  # Return the total number of EMPTY strings in the provided array using the count enumerable
+end 
